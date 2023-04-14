39cbe0a8bff8fd9976ffce76eee3022bf1b34ced
nurali <nmakhmutzhan04@gmail.com>
2023-04-14 21:05:54 +0600

    2

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         MyArrayList myArrayList = new MyArrayList();
         System.out.println(myArrayList.size());
+        myArrayList.add(123);
     }
 }
--- src/MyArrayList.java
+++ src/MyArrayList.java
@@ -3,7 +3,7 @@ public class MyArrayList implements MyList{
     private int size;
 
     public MyArrayList(){
-        this.size = 5;
+        this.size = 0;
         this.arr = new Object[5];
     }
 
@@ -14,17 +14,31 @@ public class MyArrayList implements MyList{
 
     @Override
     public boolean contains(Object o) {
+
         return false;
     }
 
     @Override
     public void add(Object item) {
+        if(size == arr.length){
+            increaseBuffer();
+        }
+        else {
+            arr[size++] = item;
+        }
 
     }
 
     @Override
     public void add(Object item, int index) {
 
+    }
+    public void increaseBuffer(){
+        Object[] newArr = new Object[arr.length * 2];
+        for(int i=0; i< arr.length; i++){
+            newArr[i]=arr[i];
+        }
+        arr = newArr;
     }
 
     @Override
