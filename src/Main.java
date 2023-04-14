f3db97c5a7f2294534b30440e21047a77ffcddcc
nurali <nmakhmutzhan04@gmail.com>
2023-04-14 20:52:27 +0600

    1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        MyArrayList myArrayList = new MyArrayList();
+        System.out.println(myArrayList.size());
     }
 }
---
+++ src/MyArrayList.java
@@ -0,0 +1,64 @@
+public class MyArrayList implements MyList{
+    private Object[] arr;
+    private int size;
+
+    public MyArrayList(){
+        this.size = 5;
+        this.arr = new Object[5];
+    }
+
+    @Override
+    public int size() {
+        return arr.length;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return false;
+    }
+
+    @Override
+    public void add(Object item) {
+
+    }
+
+    @Override
+    public void add(Object item, int index) {
+
+    }
+
+    @Override
+    public boolean remove(Object item) {
+        return false;
+    }
+
+    @Override
+    public Object remove(int index) {
+        return null;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public Object get(int index) {
+        return null;
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public void sort() {
+
+    }
+}
